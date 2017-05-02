f421edc8a93ce521640ea2fbc2aaf3962d7fcab6
inforian <ndhiman08@gmail.com>
2017-05-02 14:55:23 +0530

    Add : Dependency config added :hammer:

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from setuptools import find_packages
 setup(
   name='drf-pyotp',
   packages=find_packages(exclude=['example']),
-  version='0.3.0',
+  version='0.4.0',
   description='Rest extension of PyOTP python library, Here OTP generation and Verification is done via DRF APIs.',
   author='Neeraj Dhiman',
   author_email='ndhiman08@gmail.com',
   license='GPL',
   url='https://github.com/inforian/drf-pyotp',
-  download_url='https://github.com/inforian/drf-pyotp/archive/0.3.0tar.gz',
+  download_url='https://github.com/inforian/drf-pyotp/archive/0.4.0tar.gz',
   keywords=['otp', 'pyotp', 'totp', 'hotp', 'drf-pyotp'],
   classifiers=[],
+  install_requires=[
+    'Django>=1.9',
+    'djangorestframework>=3.0',
+    'pyotp>=2.2.4'
+  ]
 )
