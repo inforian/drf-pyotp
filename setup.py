dc5e05766a1564b9bd11b6e82c8b53273357bd1b
inforian <ndhiman08@gmail.com>
2017-05-02 16:22:30 +0530

    Fix : git release url :bug:

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 setup(
   name='drf-pyotp',
   packages=find_packages(exclude=['example']),
-  version='0.5.1',
+  version='0.5.2',
   description='Rest extension of PyOTP python library, Here OTP generation and Verification is done via DRF APIs.',
   author='Neeraj Dhiman',
   author_email='ndhiman08@gmail.com',
   license='GPL',
   url='https://github.com/inforian/drf-pyotp',
-  download_url='https://github.com/inforian/drf-pyotp/archive/0.5.1tar.gz',
+  download_url='https://github.com/inforian/drf-pyotp/archive/0.5.2.tar.gz',
   keywords=['otp', 'pyotp', 'totp', 'hotp', 'drf-pyotp'],
   classifiers=[],
   install_requires=[
